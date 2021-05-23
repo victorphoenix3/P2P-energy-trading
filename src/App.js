559906d72d637ad7c356b93fcb0df402e0ee4af0
Urjita Sharma <usharma1@ee.iitr.ac.in>
2021-05-24 00:12:30 +0530

    Modified App.js to display ETH balance and auth accessed address.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import { useWeb3 } from '@openzeppelin/network/react';
+import Web3Data from './Web3Data.jsx';
+const infuraProjectId = '3f0542feaafa428db37496e01de8cb5f';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const web3Context = useWeb3(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`);
+return (
+<div className="App">
+	<div>
+	<h1>Infura React Dapp with Components!</h1>
+	<Web3Data title="Web3 Data" web3Context={web3Context} />
+	</div>
+</div>
+);
 }
 
 export default App;
